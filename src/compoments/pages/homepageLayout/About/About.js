424386b486c9045f601dc4420dca4a3eda0b7abe
aslam48 <youngfizzy001@gmail.com>
2023-02-18 22:45:35 +0100

    ok

--- src/compoments/pages/homepageLayout/About/About.js
+++ src/compoments/pages/homepageLayout/About/About.js
@@ -26,47 +26,66 @@ const About = () => {
         <div className='blog_box'>
           <img src='https://github.com/aslam48/Retink-AI/blob/main/src/assets/logologo.png?raw=true' alt=""/>
           <small>Logod</small>
-          <span></span>
-          <p> Have a unique & creative logo designed to express and represent your brand identity.</p>
+          <span className='link-underline'></span>
+          <p>  Write SEO enriched blog posts as long or short articles on any topic of your choice.</p>
         </div>
 
         <div className='blog_box'>
           <img src='https://github.com/aslam48/Retink-AI/blob/main/src/assets/mac.png?raw=true' alt=""/>
-          <small>Logod</small>
+          <small>Copywriting</small>
           <span></span>
-          <p> Have a unique & creative logo designed to express and represent your brand identity.</p>
+          <p>Have creative and compelling copies written to boost your product, brand, service or company.</p>
         </div>
 
 
         <div className='blog_box'>
           <img src='https://github.com/aslam48/Retink-AI/blob/main/src/assets/computer.png?raw=true' alt=""/>
-          <small>Logod</small>
+          <small>Blog Writing</small>
           <span></span>
           <p> Have a unique & creative logo designed to express and represent your brand identity.</p>
         </div>
 
         <div className='blog_box'>
           <img src='https://github.com/aslam48/Retink-AI/blob/main/src/assets/camera.png?raw=true' alt=""/>
-          <small>Logod</small>
+          <small>Animated Videos</small>
           <span></span>
-          <p> Have a unique & creative logo designed to express and represent your brand identity.</p>
+          <p>Bring animated characters to life to keep your viewers engaged and  entertained.</p>
         </div>
 
         <div className='blog_box'>
           <img src='https://github.com/aslam48/Retink-AI/blob/main/src/assets/copywrite.png?raw=true' alt=""/>
-          <small>Logod</small>
+          <small>Product Demo</small>
           <span></span>
-          <p> Have a unique & creative logo designed to express and represent your brand identity.</p>
+          <p>Introduce your product to potential customers in a clear and creative video.</p>
         </div>
 
         <div className='blog_box'>
           <img src='https://github.com/aslam48/Retink-AI/blob/main/src/assets/social%20phone.png?raw=true' alt=""/>
-          <small>Logod</small>
+          <small>Social Media Ads</small>
           <span></span>
-          <p> Have a unique & creative logo designed to express and represent your brand identity.</p>
+          <p> Boost sales and awareness with tailor made ads from experts.</p>
         </div>
 
         </div>
+
+
+
+        {/* button */}
+        
+        <div className='join_btn_component'>
+            <h2 className='context_text_Title'>Sign Up For The BETA to see more</h2>
+
+           <div className='flex-btn space-x-36'>
+            <button className='btn'>Business Name</button>
+            <button className='btn'>Email</button>
+           </div>
+
+           <div className='colume-btn'>
+            {/* <button className='border border-indigo-600 bg-indigo-500 rounded   w-full p-4 my-2 text-white transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 hover:bg-indigo-600 duration-300 '>Notify me</button> */}
+            {/* <button>Sgn up as a freelance partner</button> */}
+           </div>
+        </div>
+
     </div>
   )
 }
