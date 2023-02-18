13e920dc4aecb3ff949935e2ccfc5754383a01cc
aslam48 <youngfizzy001@gmail.com>
2023-02-18 17:01:47 +0100

    working

--- src/compoments/pages/homepageLayout/cardsSection/Carditems.js
+++ src/compoments/pages/homepageLayout/cardsSection/Carditems.js
@@ -7,42 +7,39 @@ const Carditems = () => {
     <div className="container center-cards">
          <div className="card-item">
          <div className="card-box">
-            <img src='https://raw.githubusercontent.com/aslam48/Retink-AI/2d14b348b037ac9e6795832fd388e203021a625c/src/assets/artificial-intelligence-svgrepo-com%201brain.svg' alt="" width="40px"/>
+            <img src='https://raw.githubusercontent.com/aslam48/Retink-AI/2d14b348b037ac9e6795832fd388e203021a625c/src/assets/artificial-intelligence-svgrepo-com%201brain.svg' alt="" width='45px'/>
             <p className="items-center">MakeAI-assisted Content Choices</p>
             <small>Access and Order from an extensive catalogue in any language and on your budget’s terms. If unsure, our AI guides you to the best.</small>
          </div>
 
           <div className="card-box">
-          <img src='https://raw.githubusercontent.com/aslam48/Retink-AI/2d14b348b037ac9e6795832fd388e203021a625c/src/assets/artificial-intelligence-svgrepo-com%201brain.svg' alt="" width="40px"/>
-            <p className="items-center">MakeAI-assisted Content Choices</p>
-            <small>Access and Order from an extensive catalogue in any language and on your budget’s terms. If unsure, our AI guides you to the best.</small>
+          <img src='https://raw.githubusercontent.com/aslam48/Retink-AI/2d14b348b037ac9e6795832fd388e203021a625c/src/assets/Groupbrand.svg' alt="" width='45px'/>
+            <p className="items-center">Upload and Maintain your Brand Identity</p>
+            <small>Your details are translated and sent to the AI, FP, or both (per your choice) as you relax and receive continuous updates.!</small>
           </div>
 
+          <div className="card-box">
+          <img src='https://raw.githubusercontent.com/aslam48/Retink-AI/1dbbd0e7cf8ff65bfd23f36fb3b18385ab7a5759/src/assets/Vector.svg' alt="" width='45px'/>
+            <p className="items-center">Be informed as we create</p>
+            <small>Add your preferences and brand assets to ensure the contents is consistent with your brand identity. No brand asset? We can make for you!</small>
+          </div>
 
           <div className="card-box">
-          <img src='https://raw.githubusercontent.com/aslam48/Retink-AI/2d14b348b037ac9e6795832fd388e203021a625c/src/assets/artificial-intelligence-svgrepo-com%201brain.svg' alt="" width="40px"/>
-            <p className="items-center">MakeAI-assisted Content Choices</p>
-            <small>Access and Order from an extensive catalogue in any language and on your budget’s terms. If unsure, our AI guides you to the best.</small>
+          <img src='https://raw.githubusercontent.com/aslam48/Retink-AI/1dbbd0e7cf8ff65bfd23f36fb3b18385ab7a5759/src/assets/direct-customer-svgrepo-com%201.svg' alt="" width='45px'/>
+            <p className="items-center">Receive and Review</p>
+            <small>The finished content is delivered and you can make corrections that we will effect to satisfy your requirements.</small>
           </div>
 
           
           <div className="card-box">
-          <img src='https://raw.githubusercontent.com/aslam48/Retink-AI/2d14b348b037ac9e6795832fd388e203021a625c/src/assets/artificial-intelligence-svgrepo-com%201brain.svg' alt="" width="40px"/>
-            <p className="items-center">MakeAI-assisted Content Choices</p>
-            <small>Access and Order from an extensive catalogue in any language and on your budget’s terms. If unsure, our AI guides you to the best.</small>
+          <img src='https://raw.githubusercontent.com/aslam48/Retink-AI/1dbbd0e7cf8ff65bfd23f36fb3b18385ab7a5759/src/assets/Vectorpublish.svg' alt="" width='45px'/>
+            <p className="items-center">Publish and Monitor your contents’ progress</p>
+            <small>The finished content is delivered and you can make corrections that we will effect to satisfy your requirements.</small>
           </div>
 
-          <div className="card-box">
-          <img src='https://raw.githubusercontent.com/aslam48/Retink-AI/2d14b348b037ac9e6795832fd388e203021a625c/src/assets/artificial-intelligence-svgrepo-com%201brain.svg' alt="" width="40px"/>
-            <p className="items-center">MakeAI-assisted Content Choices</p>
-            <small>Access and Order from an extensive catalogue in any language and on your budget’s terms. If unsure, our AI guides you to the best.</small>
-          </div>
+          
 
-          <div className="card-box">
-          <img src='https://raw.githubusercontent.com/aslam48/Retink-AI/2d14b348b037ac9e6795832fd388e203021a625c/src/assets/artificial-intelligence-svgrepo-com%201brain.svg' alt="" width="40px"/>
-            <p className="items-center">MakeAI-assisted Content Choices</p>
-            <small>Access and Order from an extensive catalogue in any language and on your budget’s terms. If unsure, our AI guides you to the best.</small>
-          </div>
+    
      </div>
     </div>
   );
