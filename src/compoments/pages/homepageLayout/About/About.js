d0bcb5586dc9fa076d6ac0cbabd894541fecc97a
aslam48 <youngfizzy001@gmail.com>
2023-02-18 18:36:58 +0100

    working

---
+++ src/compoments/pages/homepageLayout/About/About.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './about.css'
+
+const About = () => {
+  return (
+    <div className='container place-center'>
+        <div className='space-y-16'>
+
+        <div className='context_box'>
+            <h2 className='context_text_Title'>Transform your Creation Process</h2>
+            <p className='context_text'>With a new approach to ordering content, you can now stop juggling multiple documents and meetings and start publishing content faster and on demand</p>
+        </div>
+
+        <div className='context_box'>
+            <h2 className='context_text_Title'>Activate your business growth with RetinkContent. </h2>
+            <p className='context_text'>Save time and maintain your brand identity within your budget range or sign up for affordable plans and still access multiple content services like:</p>
+        </div>
+
+        </div>
+    </div>
+  )
+}
+
+export default About
--- src/compoments/pages/homepageLayout/content/Content.js
+++ src/compoments/pages/homepageLayout/content/Content.js
@@ -1,36 +1,50 @@
-import React from 'react'
-import './content.css'
-import RetinkAvata from '../../../../assets/Retink-avata.png'
-import Video from '../Video/Video'
-import Carditems from '../cardsSection/Carditems'
+import React from "react";
+import "./content.css";
+import RetinkAvata from "../../../../assets/Retink-avata.png";
+import Video from "../Video/Video";
+import Carditems from "../cardsSection/Carditems";
+import Footer from "../footer/Footer";
+import About from '../About/About'
 
 const Content = () => {
   return (
-    <section >
-    <div className="container text_and_img_context">
-      <div>
-        <p className='text'>Get Advanced AI</p>
-        <p className='text'>+ Expert Created <span className='logo-text'>Logo</span></p>
+    <section>
+      <div className="container text_and_img_context">
+        <div>
+          <p className="text">Get Advanced AI</p>
+          <p className="text">
+            + Expert Created <span className="logo-text">Logo</span>
+          </p>
 
-        <div className='content-box'>
-          <p className='context-text'>Boost your sales <span className='indigo'>10x faster</span> with content customization by our unique partnership of <span className='indigo'>human creativity and AI optimization</span></p>
+          <div className="content-box">
+            <p className="context-text">
+              Boost your sales <span className="indigo">10x faster</span> with
+              content customization by our unique partnership of{" "}
+              <span className="indigo">
+                human creativity and AI optimization
+              </span>
+            </p>
+          </div>
+        </div>
+
+        <div>
+          <img src={RetinkAvata} alt="icon retink" className="retink_icon" />
         </div>
       </div>
 
-      <div>
-        <img src={RetinkAvata} alt='icon retink' className='retink_icon'/>
-      </div>
-    </div>
+      {/* video */}
+      <Video />
 
-    {/* video */}
-    <Video />
+      {/* Rethinks cards  */}
+      <Carditems />
 
-    {/* rethinks cards  */}
+      {/* about */}
+      <About />
 
-    <Carditems />
-
+      {/* Footer */}
+      <Footer />
     </section>
-  )
-}
+  );
+};
 
-export default Content
+export default Content;
